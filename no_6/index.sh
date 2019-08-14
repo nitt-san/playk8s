e39710444757b92104de81a4b5cc4beeed528389
nitt_san <nitt_san@gmail.com>
2019-08-14 17:42:30 +0900

    completed until 6.6

---
+++ no_6/index.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for PODNAME in `kubectl get pods -l app=sample-app -o jsonpath='{.items[*].metadata.name}'`; do
+    kubectl exec -it ${PODNAME} -- cp /etc/hostname /usr/share/nginx/html/index.html;
+done
